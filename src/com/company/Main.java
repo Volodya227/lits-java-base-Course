8b9108a1334a01751aeb5d285dec18f589dc8034
volodya227 <balzammist2018@gmail.com>
2019-10-03 19:52:32 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//  від 0 до 50
+        var x = 'w';
+        var i = 0;
+        for(i = 0;i<=50;i++){
+            x = (char)i;
+            System.out.println(x+" = "+i);
+        }
+    }
+}
