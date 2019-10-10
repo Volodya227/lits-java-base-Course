3d6c7b928edc00ee762d14be75e8d89709ad601b
volodya227 <balzammist2018@gmail.com>
2019-10-10 09:48:38 +0300

    Homework4var2

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -8,41 +8,38 @@ public class Array {
     public int[]arr;// = new int[10];
 
     public void print() {
-        if (true) {
-            System.out.println((Arrays.toString(arr)));
-        }
+        System.out.println((Arrays.toString(arr)));
+
     }
     public void sort(){
-        if(true){
-            int [] mas = arr;//this.arr;//{9,4,6,3,2,4,5,1,2};
-            boolean isSorted = false;
-            int buf;
-            while(!isSorted) {
-                isSorted = true;
-                for (int i = 0; i < mas.length-1; i++) {
-                    if(mas[i] > mas[i+1]){
-                        isSorted = false;
 
-                        buf = mas[i];
-                        mas[i] = mas[i+1];
-                        mas[i+1] = buf;
-                    }
+        int [] mas = arr;//this.arr;//{9,4,6,3,2,4,5,1,2};
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
                 }
             }
         }
+
     }
 
-    public Array() {
-    }
-    public void spam(int len,int max){
+    public Array(int len) {
         this.arr =new int[len];
-        for(var i= 0;i<len;i++){
-         // System.out.println(Arrays.toString(this.arr));
-            //r a= 0;
-            this.arr[i]= new Random().nextInt(max);
+    }
+    public void spam(int max){
+
+        for(var i= 0;i<arr.length;i++){
+            arr[i]= new Random().nextInt(max);
         }
-        //System.out.println(Arrays.toString(this.arr));
-        var x = this.arr;
+
 
     }
     public void udit(String anction){
@@ -52,7 +49,7 @@ public class Array {
                     System.out.print(arr[i]+"  ");
                 }
             }
-        }else if(anction=="guys") {
+        }else if(anction=="not even") {
                 for (var i = 0; i < arr.length; i++) {
                     if (i % 2 == 1) {
                         System.out.print(arr[i]+"  ");
@@ -77,11 +74,5 @@ public class Array {
                 }
             }
         }
-
-
-
-
-
-
 }
 
--- src/com/company/Homework_4.java
+++ src/com/company/Homework_4.java
@@ -4,12 +4,12 @@ public class Homework_4 {
     public static void main(String[] args) {
 
 
-        var numbers = new Array();
-        numbers.spam(5,100);
+        var numbers = new Array(8);
+        numbers.spam(100);
         numbers.print();
         numbers.sort();
         numbers.print();
-        numbers.udit("even");
+        numbers.udit("not even");
         numbers.r("even",4);
         numbers.print();
         numbers.r("not even",5);
