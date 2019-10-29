c83eb71ec4dccc798cbe5fde07019b2a8a6c183c
volodya227 <balzammist2018@gmail.com>
2019-10-29 11:21:15 +0200

    homework9_variable:3

--- src/com/homework9/Main.java
+++ src/com/homework9/Main.java
@@ -2,7 +2,7 @@ package com.homework9;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+\\
 public class Main {
     public static void main(String[] args){
         a("+38(044)-228-32-27");
